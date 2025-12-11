55b182b94a3bc8f141210fce515fd66f11193795
kmp-gitty <katoa.ahau@gmail.com>
2025-12-11 06:22:12 -1000

    Add robots file

---
+++ src/app/robot.js
@@ -0,0 +1,12 @@
+export default function robots() {
+    return {
+      rules: [
+        {
+          userAgent: "*",
+          allow: "/",
+        },
+      ],
+      sitemap: "https://www.ads4good.com/sitemap.xml",
+    };
+  }
+  
